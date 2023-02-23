c4633bbe6656ba49372eaeb7252c4b6350ffe538
Takayuki Shimada <taka@tsmd.jp>
2023-02-24 07:35:55 +0900

    feat: inlay controller

---
+++ src/inlay/inlay.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus";
+import { tabbable } from "../helpers.js";
+
+/**
+ * @property {HTMLElement} containerTarget
+ * @property {boolean} hasContentTarget
+ * @property {HTMLTemplateElement} contentTarget
+ * @property {boolean} hasButtonContainerTarget
+ * @property {HTMLElement} buttonContainerTarget
+ */
+export class InlayController extends Controller {
+  static targets = ["container", "content", "buttonContainer"];
+
+  show() {
+    if (this.hasContentTarget) {
+      const contentToAppend = this.contentTarget.content;
+      const focusTo = tabbable(contentToAppend)[0];
+      this.containerTarget.appendChild(contentToAppend);
+      this.contentTarget.remove();
+      focusTo?.focus({ preventScroll: true });
+
+      if (!this.hasContentTarget && this.hasButtonContainerTarget) {
+        this.buttonContainerTarget.remove();
+      }
+    }
+  }
+}
